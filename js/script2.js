21814690a756e23a60fb12e0773e14f29005c4a4
Flavio <flavio.origgi@outlook.it>
2021-06-16 14:57:23 +0200

    finito anche il 2°

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,7 @@
 //* Creare un array di oggetti di squadre di calcio.
 //* Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //* Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-var squadre = [
+const squadre = [
     {
         nome : 'milan',
         puntiFatti : 0,
@@ -20,16 +20,20 @@ var squadre = [
 ];  
 
 //* Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (utilizzate le arrow function). 
-const numeriRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const nuovoArr = [];
 
+const numeriRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 for (let i = 0; i < squadre.length; i++){
     let puntiFattiRandom = numeriRandom(0, 3);
     let falliSubitiRandom = numeriRandom(0, 5);
     squadre[i].puntiFatti = puntiFattiRandom;
     squadre[i].falliSubiti = falliSubitiRandom;
+    //* Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e 
+    //* falli subiti e stampiamo tutto in console. 
+    const {nome, falliSubiti} = squadre[i];
+    nuovoArr.push({nome, falliSubiti});
 }
-console.log(squadre);
+console.log(nuovoArr);
 
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e 
-// falli subiti e stampiamo tutto in console.
+
 
